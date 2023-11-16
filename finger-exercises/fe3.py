e088bf5d4b9966da5d7b202175c91a7610f8f91c
ezratweaver <ezratweaver@gmail.com>
2023-11-15 21:20:12 -0600

    Finger Exercise 3 Commit

---
+++ finger-exercises/fe3.py
@@ -0,0 +1,7 @@
+num_x = int(input('How many times should I print the letter X? '))
+to_print = ''
+iteration = 0
+while iteration < num_x:
+    to_print = to_print + 'X\n'
+    iteration += 1
+print(to_print)
