05e3e034b7db5d2bec58ac4fca8f63e833c9d363
ezratweaver <ezratweaver@gmail.com>
2023-11-17 16:26:58 -0600

    Added documentation

--- finger-exercises/Chapter 2/fe4.py
+++ finger-exercises/Chapter 2/fe4.py
@@ -1,3 +1,8 @@
+"""Finger exercise: Write a program that asks the user to input 10
+integers, and then prints the largest odd number that was entered. If
+no odd number was entered, it should print a message to that effect."""
+
+
 def largest_odd_number(*args):
     divisible_by_3 = []
     for number in args:
@@ -10,4 +15,4 @@ def largest_odd_number(*args):
     return return_value
 
 
-print(largest_odd_number(6, 4, 2, 8, 10, 12, 4))
+print(largest_odd_number(6, 4, 2, 8, 10, 12, 4, 11, 454, 195))
