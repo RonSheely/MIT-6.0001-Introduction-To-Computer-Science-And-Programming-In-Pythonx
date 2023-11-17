626ae2ebc2fcf13235da277a976ba058b8a5d1ec
ezratweaver <ezratweaver@gmail.com>
2023-11-17 15:59:42 -0600

    Finger Exercise 7 Commit

--- finger-exercises/Chapter 3/fe7.py
+++ finger-exercises/Chapter 3/fe7.py
@@ -3,3 +3,23 @@ integer and prints two integers, root and pwr, such that 1 < pwr < 6
 and root**pwr is equal to the integer entered by the user. If no such
 pair of integers exists, it should print a message to that effect."""
 
+
+def get_root_and_power(user_number: int) -> tuple:
+    for x in range(2, user_number):
+        for y in range(2, 7):
+            if x**y == user_number:
+                return x, y
+    return 0, 0
+
+
+def main():
+    gain_input = int(input("Please enter a number to be tested: "))
+    result = get_root_and_power(gain_input)
+    if result == (0, 0):
+        print("root**power does not exist for your integer, unless power > 6")
+    else:
+        print(f"The resulting equation to your number is {result[0]}**{result[1]}")
+
+
+if __name__ == "__main__":
+    main()
