23f5dca5bb9f782f9d4af485c1cb58f9df6c15bf
ezratweaver <ezratweaver@gmail.com>
2023-11-17 16:26:54 -0600

    Added documentation

--- finger-exercises/Chapter 2/fe2.py
+++ finger-exercises/Chapter 2/fe2.py
@@ -1,3 +1,8 @@
+"""Finger exercise: Write code that asks the user to enter their
+birthday in the form mm/dd/yyyy, and then prints a string of the
+form ‘You were born in the year yyyy.’"""
+
+
 user_date = input("What is your date of birth? (MM/DD/YYYY): ")
 if len(user_date) > 10:
     raise ValueError("Input Too Long")
