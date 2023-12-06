e13fa495bbffa20b89077981ac7f1fbb773cc685
ezratweaver <ezratweaver@gmail.com>
2023-12-05 23:08:36 -0600

    Problem Set 1 A Commit

---
+++ problem-sets/ps1/ps1a.py
@@ -0,0 +1,24 @@
+G_PORTION_DOWN_PAYMENT = 0.25
+G_ANNUAL_RETURN = 0.04
+
+
+def months_to_save(annual_salary: int, portion_saved: float, total_cost: int) -> int:
+    money_to_save = total_cost * G_PORTION_DOWN_PAYMENT
+    current_savings = 0
+    months_passed = 0
+    while current_savings < money_to_save:
+        months_passed += 1
+        return_on_investments = round(current_savings * G_ANNUAL_RETURN / 12, 2)
+        annual_salary_portion = round(annual_salary * portion_saved / 12, 2)
+        current_savings += return_on_investments
+        current_savings += annual_salary_portion
+    return months_passed
+
+
+input_annual_salary = int(input("Enter your annual salary: "))
+input_portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+input_total_cost = int(input("Enter the cost of your dream home: "))
+
+number_of_months = months_to_save(input_annual_salary, input_portion_saved, input_total_cost)
+
+print(f"Number of months: {number_of_months}")
