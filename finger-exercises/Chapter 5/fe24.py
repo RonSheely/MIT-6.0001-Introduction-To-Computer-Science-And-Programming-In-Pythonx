59f62ac8ec93127571a18f368a1b8964dd873e0c
ezratweaver <ezratweaver@gmail.com>
2023-12-28 17:07:11 -0600

    Tweaks to finger exercise 24

--- finger-exercises/Chapter 5/fe24.py
+++ finger-exercises/Chapter 5/fe24.py
@@ -14,6 +14,8 @@ buckler, a lean hack, and a greyhound for coursing. """
 
 SECRET_MESSAGE = "22*13*33*137*59*11*23*11*1*57*6*13*1*2*6*57*2*6*1*22*13*33*137*59*11*23*11*1*57*6*173*7*11"
 
+PLAIN_TEXT = "comprehension is incomprehensible"
+
 gen_code_keys = (lambda book, plain_text: ({c: str(book.find(c)) for c in plain_text}))
 
 gen_decode_keys = (lambda book, cipher_text: {s: book[int(s)] for s in cipher_text.split('*')})
@@ -22,8 +24,8 @@ encoder = (lambda code_keys, plain_text: ''.join(['*' + code_keys[c] for c in pl
 
 decoder = (lambda decode_keys, cipher_text: ''.join(decode_keys[s] for s in cipher_text.split('*')))
 
-gen_code = gen_code_keys(DON_QUIXOTE, "comprehension is incomprehensible")
-coded_message = encoder(gen_code, "comprehension is incomprehensible")
+gen_code = gen_code_keys(DON_QUIXOTE, PLAIN_TEXT)
+coded_message = encoder(gen_code, PLAIN_TEXT)
 
 gen_decode = gen_decode_keys(DON_QUIXOTE, SECRET_MESSAGE)
 decoded_message = decoder(gen_decode, SECRET_MESSAGE)
