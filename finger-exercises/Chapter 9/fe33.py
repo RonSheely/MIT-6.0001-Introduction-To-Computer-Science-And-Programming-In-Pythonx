23fc8f065e102aa7a3a6837980c14f4e6f24261c
ezratweaver <ezratweaver@gmail.com>
2024-01-04 23:43:49 -0600

    Finger exercise 33 commit

---
+++ finger-exercises/Chapter 9/fe33.py
@@ -0,0 +1,64 @@
+"""Finger exercise: Add a method satisfying the specification below
+to the Int_set class."""
+
+
+class Int_set(object):
+    """An Int_set is a set of integers"""
+
+    def __init__(self):
+        """Create an empty set of integers"""
+        self._vals = []
+
+    def insert(self, e):
+        """Assumes e is an integer and inserts e into self"""
+        if e not in self._vals:
+            self._vals.append(e)
+
+    def member(self, e):
+        """Assumes e is an integer
+        Returns True if e is in self, and False otherwise"""
+        return e in self._vals
+
+    def remove(self, e):
+        """Assumes e is an integer and removes e from self
+        Raises ValueError if e is not in self"""
+        try:
+            self._vals.remove(e)
+        except ValueError:
+            raise ValueError(str(e) + ' not found')
+
+    def get_members(self):
+        """Returns a list containing the elements of self._
+        Nothing can be assumed about the order of the elements"""
+        return self._vals[:]
+
+    def __add__(self, other):
+        """other is an Int_set
+        mutates self so that it contains exactly the
+        elemnts in self
+        plus the elements in others."""
+        for num in other._vals[:]:
+            if num not in self._vals:
+                self._vals.append(num)
+
+    def __str__(self):
+        """Returns a string representation of self"""
+        if not self._vals:
+            return '{}'
+        self._vals.sort()
+        result = ''
+        for e in self._vals:
+            result = result + str(e) + ','
+        return f"{{{result[:-1]}}}"
+
+
+x = Int_set()
+y = Int_set()
+
+x.insert(1)
+x.insert(2)
+y.insert(2)
+y.insert(3)
+
+x + y
+print(x)
