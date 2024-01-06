9c985ad68615b74f92ad72ac2e8c1c9969003343
ezratweaver <ezratweaver@gmail.com>
2024-01-05 19:04:21 -0500

    Problem Set 4-A commit

--- problem-sets/ps4/ps4a.py
+++ problem-sets/ps4/ps4a.py
@@ -1,14 +1,22 @@
 # Problem Set 4A
-# Name: <your name here>
+# Name: <Ezra Weaver>
 # Collaborators:
 # Time Spent: x:xx
 
+def add_char(string, char):
+    empty_list = [char + string]
+    for x in range(len(string)):
+        new_string = string[x:] + char + string[:x]
+        empty_list.append(new_string)
+    return empty_list
+
+
 def get_permutations(sequence):
-    '''
+    """
     Enumerate all permutations of a given string
 
     sequence (string): an arbitrary string to permute. Assume that it is a
-    non-empty string.  
+    non-empty string.
 
     You MUST use recursion for this part. Non-recursive solutions will not be
     accepted.
@@ -21,20 +29,21 @@ def get_permutations(sequence):
 
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
-    '''
+    """
+    if len(sequence) == 1:
+        return [sequence]
+    char = sequence[0]
+    sample = get_permutations(sequence[1:])
+    final_list = []
+    for x in sample:
+        final_list = final_list + add_char(x, char)
+    return final_list
 
-    pass #delete this line and replace with your code here
 
 if __name__ == '__main__':
-#    #EXAMPLE
-#    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-#    print('Actual Output:', get_permutations(example_input))
-    
-#    # Put three example test cases here (for your sanity, limit your inputs
-#    to be three characters or fewer as you will have n! permutations for a 
-#    sequence of length n)
+    example_input = 'abc'
+    print('Input:', example_input)
+    print('Expected Output:', ['abc', 'bca', 'cab', 'acb', 'cba', 'bac'])
+    print('Actual Output:', get_permutations(example_input))
 
-    pass #delete this line and replace with your code here
 
