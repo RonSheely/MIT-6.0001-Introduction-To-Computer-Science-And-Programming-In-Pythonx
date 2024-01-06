38b0ef50a09ba868d9f4210bb88041281a7673f3
ezratweaver <ezratweaver@gmail.com>
2024-01-05 22:22:34 -0500

    build_shift_dict function commit

--- problem-sets/ps4/ps4b.py
+++ problem-sets/ps4/ps4b.py
@@ -2,6 +2,16 @@
 # Name: <your name here>
 # Collaborators:
 # Time Spent: x:xx
+import string
+
+ALPHABET_DICT = {'a': 27, 'b': 28, 'c': 29, 'd': 30, 'e': 31, 'f': 32, 'g': 33,
+                 'h': 34, 'i': 35, 'j': 36, 'k': 37, 'l': 38, 'm': 39, 'n': 40,
+                 'o': 41, 'p': 42, 'q': 43, 'r': 44, 's': 45, 't': 46, 'u': 47,
+                 'v': 48, 'w': 49, 'x': 50, 'y': 51, 'z': 52, 'A': 1, 'B': 2,
+                 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10,
+                 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17,
+                 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
+ALPHABET_DICT_SWAPPED = {v: k for k, v in ALPHABET_DICT.items()}
 
 
 def load_words(file_name):
@@ -98,7 +108,14 @@ class Message(object):
         Returns: a dictionary mapping a letter (string) to
                  another letter (string).
         """
-        pass  # delete this line and replace with your code here
+        assert 0 <= shift < 26, "Letter shift must be (0 <= shift < 26)"
+        shift_dict = dict()
+        for letter, value in ALPHABET_DICT.items():
+            new_value = value + shift
+            if new_value > 52:
+                new_value -= 52
+            shift_dict[letter] = ALPHABET_DICT_SWAPPED[new_value]
+        return shift_dict
 
     def apply_shift(self, shift):
         """
@@ -217,4 +234,5 @@ if __name__ == '__main__':
 
     # TODO: best shift value and unencrypted story
 
-    pass  # delete this line and replace with your code here
+    x = Message("Hello World")
+    print(x.build_shift_dict(4))
