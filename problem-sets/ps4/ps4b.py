249cd9dc6795830b6b97dc9d037b7788475a544e
ezratweaver <ezratweaver@gmail.com>
2024-01-05 23:15:44 -0500

    Styling changes and TODO list update

--- problem-sets/ps4/ps4b.py
+++ problem-sets/ps4/ps4b.py
@@ -230,17 +230,19 @@ class CiphertextMessage(Message):
 
 
 if __name__ == '__main__':
-       #Example test case (PlaintextMessage)
-       plaintext = PlaintextMessage('hello', 2)
-       print('Expected Output: jgnnq')
-       print('Actual Output:', plaintext.get_message_text_encrypted())
+    # Example test case (PlaintextMessage)
+    plaintext = PlaintextMessage('hello', 2)
+    print('Expected Output: jgnnq')
+    print('Actual Output:', plaintext.get_message_text_encrypted())
 
-       #Example test case (CiphertextMessage)
-       ciphertext = CiphertextMessage('jgnnq')
-       print('Expected Output:', (24, 'hello'))
-       print('Actual Output:', ciphertext.decrypt_message())
+    # Example test case (CiphertextMessage)
+    ciphertext = CiphertextMessage('jgnnq')
+    print('Expected Output:', (24, 'hello'))
+    print('Actual Output:', ciphertext.decrypt_message())
 
-    # TODO: WRITE YOUR TEST CASES HERE
 
-    # TODO: best shift value and unencrypted story
+# TODO: WRITE decrypt_message METHOD IN CIPHERTEXTMESSAGE CLASS
 
+# TODO: WRITE YOUR TEST CASES HERE
+
+# TODO: best shift value and unencrypted story
